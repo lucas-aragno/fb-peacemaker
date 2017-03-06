73e576be5867e85a3188e110915eea583730d35f
lucas <lucas.aragno157@gmail.com>
2017-03-05 22:23:15 -0300

    beautify

--- peacemaker.js
+++ peacemaker.js
@@ -1,67 +1,62 @@
 if (document.URL.indexOf('facebook') != -1) {
-  walk(document.body);
+    walk(document.body);
 }
 
-function walk(node)
-{
+function walk(node) {
 
-	var child, next;
-	if (node.tagName && (node.tagName.toLowerCase() === 'input' || node.tagName.toLowerCase() === 'textarea')) {
-		return;
-	}
+    var child, next;
+    if (node.tagName && (node.tagName.toLowerCase() === 'input' || node.tagName.toLowerCase() === 'textarea')) {
+        return;
+    }
 
-	switch ( node.nodeType )
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child )
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+    switch (node.nodeType) {
+        case 1: // Element
+        case 9: // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
 }
 
-function handleText(textNode)
-{
-	var v = textNode.nodeValue;
+function handleText(textNode) {
+    var v = textNode.nodeValue;
 
-  var stupidArguments = [
-    'macri',
-    'cristina kirchner',
-    'presidenta'
-  ]
+    var stupidArguments = [
+        'macri',
+        'cristina kirchner',
+        'presidenta'
+    ]
 
-  var lyrics = [
-    `Somebody once told me the world is gonna roll me
+    var lyrics = [
+        `Somebody once told me the world is gonna roll me
      I ain't the sharpest tool in the shed
      She was looking kind of dumb with her finger and her thumb
      In the shape of an "L" on her forehead`,
-     `If there's anything that you want
+        `If there's anything that you want
       If there's anything I can do
       Just call on me and I'll send it along
       With love from me to you`,
-      `You've been hit by
+        `You've been hit by
        You've been struck by
        A smooth criminal`,
-      `One, two, three!
+        `One, two, three!
        My baby don't mess around
        Because she loves me so
        This I know fo sho!
       `
-  ]
+    ]
 
-  stupidArguments.forEach(function(element) {
-    if (v.toLowerCase().indexOf(element) != -1) {
-      textNode.nodeValue = lyrics[Math.floor(Math.random() * lyrics.length)]
-    }
-  })
-}
+    stupidArguments.forEach(function(element) {
+        if (v.toLowerCase().indexOf(element) != -1) {
+            textNode.nodeValue = lyrics[Math.floor(Math.random() * lyrics.length)]
+        }
+    })
